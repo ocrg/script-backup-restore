3824658a23d2c09635e43435f8d1c0f20bc8984b
Grasset <poubellamel@icloud.com>
2021-09-11 17:12:39 +0200

    Mise en place de gitignore

--- svg.py
+++ svg.py
@@ -1,5 +1,7 @@
-# Projet 6, sauvegarde wordpress.
+# Projet 6 et 9, sauvegarde wordpress.
 
+def svg(wp,wp1,wp2,sql0)
+# wp0 wp1 wp2 et sql0 auront peut-être une utilité, peut-être pas, on verra.
 création d'un dossier
 copie des différents fichiers 1 par 1 dans lesdits dossiers
 
