d924d92af6e2e128ca61e8f5dcde5f2fce587a15
Grasset <poubellamel@icloud.com>
2021-09-11 23:16:40 +0200

    Import os, je repars de zéro pour l'algo, il est plus lisible.

--- svg.py
+++ svg.py
@@ -1,11 +1,19 @@
 # Projet 6 et 9, sauvegarde wordpress.
 
 def svg(wp,wp1,wp2,sql0)
+# Les imports
+import os
+
 # wp0 wp1 wp2 et sql0 auront peut-être une utilité, peut-être pas, on verra.
-création d'un dossier
+os.mkdir('/home/rick/ZZZ')
 copie des différents fichiers 1 par 1 dans lesdits dossiers
 
 compression en tar.machin
 contrôle du tar.machin
 
 idée : sauvegarde incrémenble...
+files_to_save = ['/var/www/html/', '/var/www/html/','/var/www/html/','/var/www/html/','/var/www/html/']
+
+
+
+for 
