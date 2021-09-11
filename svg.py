0d3fe7ad5cbf0934dd770f253e54d425fa8637a6
Jeremy Grasset <poubellamel@icloud.com>
2021-09-11 16:39:52 +0200

    Rerererere commit soit disant initial

---
+++ svg.py
@@ -0,0 +1,9 @@
+# Projet 6, sauvegarde wordpress.
+
+création d'un dossier
+copie des différents fichiers 1 par 1 dans lesdits dossiers
+
+compression en tar.machin
+contrôle du tar.machin
+
+idée : sauvegarde incrémenble...
