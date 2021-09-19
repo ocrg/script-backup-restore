b3747c4c1305e6361fdff30dafbc6ef9e6440f6f
Grasset <poubellamel@icloud.com>
2021-09-19 17:00:36 +0200

    Remise en forme du code, optimisation de sa lisibilité, ajout d'instructions pour supprimer le dossier 'tampon'.

--- svg.py
+++ svg.py
@@ -7,24 +7,21 @@ import tarfile
 
 # On fait une fonction.
 def svg():
-# Après les tests, le contenu du tableau files_to_save sera le commentaire ci-dessous :
-#	'/var/www/html/www.ocr.tp/wp-config-sample.php', '/var/www/html/www.ocr.tp/wp-config.php','/var/www/html/www.ocr.tp/wp-content','/var/www/html/www.ocr.tp/.htaccess','extract sql'
 
-
-	#############	OK	################
 	# Création du dossier pour travailler dedans par simplicité.
-	if os.path.exists('/home/rick/Backup_P6') == False:
-		os.mkdir('/home/rick/Backup_P6')
+	if os.path.exists('/home/debian/Backup_P6') == False:
+		os.mkdir('/home/debian/Backup_P6')
 	else:
 		print("Le dossier 'Backup_P6' existe déjà.")
 
-	# On met tous les fichiers dans le tableau et on fait de sa première entrée une variable.
-	files_to_save = ['/home/rick/Bureau/ocr.tp', '/home/rick/Bureau/OCR', '/home/rick/Bureau/Chill.xspf', '/home/rick/Bureau/Améliorer Gnome sur Debian', '/home/rick/Bureau/Pognon.ods', '/home/rick/Bureau/Linux.odt']
+	# Variable du dossier.
+	directory_where_save = '/home/debian/Backup_P6/'
 
-	# Idem avec notre répertoire de sauvegarde.
-	directory_where_save = '/home/rick/Backup_P6/'
+	# On met tous les fichiers à sauvegarder dans un tableau.
+	files_to_save = ['/var/www/html/www.ocr.tp/wp-config.php','/var/www/html/www.ocr.tp/wp-content','/var/www/html/www.ocr.tp/.htaccess']
 
-	# Tant que le tableau n'est pas vide, on copie de l'entrée 0.
+	# On fait le tour du tableau.
+	print("Début de la copie.")
 	for file_or_dir in files_to_save:
 		# On contrôle si c'est un dossier ou un fichier.
 		if os.path.isdir(file_or_dir) == True and os.path.exists(directory_where_save + os.path.basename(file_or_dir)) == False:
@@ -37,29 +34,30 @@ def svg():
 			print("Ce fichier existe déjà dans le dossier de destination.")			
 		else:
 			print("Il y a eu un problème avec", file_or_dir)
-
-	print("Dossier terminé.")
-
-	#############	OK	################
+	print("Fin de la copie.")
 
 # mysqldump à mettre en python
-#mysqldump -u root -p wordpress --databases wordpress > /home/debian/svg.sql
+# mysqldump -u root -p wordpress --databases wordpress > /home/debian/svg.sql
 
 # en argument le dossier Backup_P6 qui serai utilisé pour tarfile
 
-########### Compression, c'est clé en main, à adapter.
-#def make_tarfile(output_filename, source_dir):
+# Compression, c'est clé en main, à adapter.
+# def make_tarfile(output_filename, source_dir):
 #    with tarfile.open(output_filename, "w:gz") as tar:
 #        tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 	output_filename = 'Backup_P6.tar.gz'
-	if os.path.isfile('/home/rick/' + output_filename) == True:
+	print("Début de la compression.")
+	if os.path.isfile('/home/debian/' + output_filename) == True:
 		print("Le fichier existe déjà.")
 	else:
-		with tarfile.open('/home/rick/' + output_filename, "w:gz") as tar:
-			tar.add(directory_where_save, arcname = os.path.basename(directory_where_save))
+		# Instruction pour le tar.gz.
+		with tarfile.open('/home/debian/' + output_filename, "w:gz") as tar:
+			tar.add(directory_where_save, os.path.basename(directory_where_save))
+	print("Compression terminée.")
 
-	print("Archive terminée.")
+	shutil.rmtree(directory_where_save)
+	print("Backup terminée avec succès")
 
 # Appel de la fonction.
 svg()
