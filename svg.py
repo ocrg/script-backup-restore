593192aef60afeadd1f0ceb36ba77b755d34ff1f
Grasset <poubellamel@icloud.com>
2021-09-19 19:00:51 +0200

    Tests et optimisations du code pour le backup de la base MySQL OK ! Améliorations mineures. Argument 'temp_dir' à entamer demain.

--- svg.py
+++ svg.py
@@ -1,5 +1,7 @@
 # Projet 6 et 9, sauvegarde wordpress.
 
+# en argument le dossier Backup_P6 qui serai utilisé pour tarfile
+
 # Les imports
 import os
 import shutil
@@ -7,32 +9,35 @@ import tarfile
 import pipes
 
 # On fait une fonction.
-def svg():
+def svg(temp_dir):
 
-	# Création du dossier pour travailler dedans par simplicité.
+	# Création du dossier temporaire pour travailler dedans par simplicité.
 	if os.path.exists('/home/debian/Backup_P6') == False:
 		os.mkdir('/home/debian/Backup_P6')
 	else:
-		print("Le dossier 'Backup_P6' existe déjà.")
+		print("Le dossier temporaire 'Backup_P6' existe déjà.")
 
-	# Variable du dossier.
+	# Variable du dossier temporaire.
 	directory_where_save = '/home/debian/Backup_P6/'
 
+	### MySQLdump, début. ###
+	# La ligne ci-dessous produit le même résultat si on la saisie : mysqldump -u root -p wordpress --databases wordpress > /home/debian/svg.sql
+	# Les constantes
 	DB_HOST = 'localhost' 
 	DB_USER = 'root'
 	DB_USER_PASSWORD = 'debian'
 	DB_NAME = 'wordpress'
 	BACKUP_PATH = '/home/debian/Backup_P6/'
 
-	dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + DB_NAME + " > " + pipes.quote(BACKUP_PATH) + "/" + DB_NAME + ".sql"
-
-	print(dumpcmd)
+	# La ligne de code.
+	dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + DB_NAME + " > " + pipes.quote(BACKUP_PATH) + DB_NAME + ".sql"
 	os.system(dumpcmd)
+	# Un print pour voir ce qui est clairement saisie.
+	print(dumpcmd)
+	### MySQLdump, fin. ###
 
-	print ("La BDD a été sauvegardée ici : " + BACKUP_PATH)
-
-	# On met tous les fichiers à sauvegarder dans un tableau.
-	files_to_save = ['/var/www/html/www.ocr.tp/wp-config.php','/var/www/html/www.ocr.tp/wp-content','/var/www/html/www.ocr.tp/.htaccess', '/home/debian/Backup_P6/' + DB_NAME + '.sql']
+	# On met tous les fichiers à sauvegarder dans un tableau. Le fichier SQL est déjà dans le dossier temporaire.
+	files_to_save = ['/var/www/html/www.ocr.tp/wp-config.php','/var/www/html/www.ocr.tp/wp-content','/var/www/html/www.ocr.tp/.htaccess']
 
 	# On fait le tour du tableau.
 	print("Début de la copie.")
@@ -43,35 +48,28 @@ def svg():
 		elif os.path.isfile(file_or_dir) == True and os.path.exists(directory_where_save + os.path.basename(file_or_dir)) == False:
 			shutil.copyfile(file_or_dir, directory_where_save + os.path.basename(file_or_dir))
 		elif os.path.isdir(file_or_dir) == True and os.path.exists(directory_where_save + os.path.basename(file_or_dir)) == True:
-			print("Ce dossier existe déjà dans le dossier de destination.")
+			print("Ce dossier existe déjà dans le dossier temporaire : " + file_or_dir)
 		elif os.path.isfile(file_or_dir) == True and os.path.exists(directory_where_save + os.path.basename(file_or_dir)) == True:
-			print("Ce fichier existe déjà dans le dossier de destination.")			
+			print("Ce fichier existe déjà dans le dossier temporaire : " + file_or_dir)			
 		else:
-			print("Il y a eu un problème avec", file_or_dir)
+			print("Il y a eu un problème avec" + file_or_dir)
 	print("Fin de la copie.")
 
-# mysqldump à mettre en python
-# mysqldump -u root -p wordpress --databases wordpress > /home/debian/svg.sql
-
-# en argument le dossier Backup_P6 qui serai utilisé pour tarfile
-
-# Compression, c'est clé en main, à adapter.
-# def make_tarfile(output_filename, source_dir):
-#    with tarfile.open(output_filename, "w:gz") as tar:
-#        tar.add(source_dir, arcname=os.path.basename(source_dir))
-
+	# La compression.
 	output_filename = 'Backup_P6.tar.gz'
-	print("Début de la compression.")
+
 	if os.path.isfile('/home/debian/' + output_filename) == True:
 		print("Le fichier existe déjà.")
 	else:
 		# Instruction pour le tar.gz.
+		print("Début de la compression.")
 		with tarfile.open('/home/debian/' + output_filename, "w:gz") as tar:
 			tar.add(directory_where_save, os.path.basename(directory_where_save))
-	print("Compression terminée.")
+		print("Compression terminée.")
 
+	# Suppression du dossier temporaire.
 	shutil.rmtree(directory_where_save)
-	print("Backup terminée avec succès")
+	print("Backup terminée avec succès !")
 
 # Appel de la fonction.
 svg()
