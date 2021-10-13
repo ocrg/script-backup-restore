35eff69df793d4073a4c366cc04efa6f5b321fb8
Grasset <poubellamel@icloud.com>
2021-10-13 22:37:49 +0200

    Ajout de détail dans config.yaml ; revue de svg.py, ajout de petits détails correction de bugs que je n'avais pas remarqués, rien de très important du reste.

--- svg.py
+++ svg.py
@@ -16,7 +16,7 @@ import tarfile
 # yaml nécessite d'installer "pip" puis de faire "sudo pip install PyYAML"
 import yaml
 import sys
-# subprocess execute une ligne de commande comme dans un terminal.
+# subprocess exécute une ligne de commande comme dans un terminal.
 import subprocess
 
 
@@ -28,26 +28,20 @@ import subprocess
 
 
 ### LECTURE DU FICHIER YAML ###
-#try:
-	# Récupération du contenu du yaml.
-#	with open("config.yaml", 'r') as stream:
-#		vars = yaml.safe_load(stream)
-#except:
-#	exit("Pas de fichier du nom de config.yaml")
-
 def readConf(file):
 	try:
 		with open(file, 'r') as Stream:
 			return yaml.safe_load(Stream)
 	except:
-		exit(1)
+		print("Problème de lecture du fichier .yaml.")
+		exit(0)
 
 
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### CODE DE LA PARTIE BACKUP ###
 
 
 
@@ -64,7 +58,6 @@ def create_tmp_save(deb, name_of_backup, temp_directory):
 		os.mkdir(temp_directory)
 
 
-
 ### SQL_DUMP ###
 def sql_save(DB_HOST, DB_USER, DB_USER_PASSWORD, DB_NAME, BACKUP_PATH):
 	try:
@@ -76,9 +69,8 @@ def sql_save(DB_HOST, DB_USER, DB_USER_PASSWORD, DB_NAME, BACKUP_PATH):
 		subprocess.run(dumpcmd, shell=True)
 	except:
 		shutil.rmtree(temp_directory)
-		# MySQLdump peut générer des erreurs mais poursuivre correctement malgré tout...
-		exit("Problème avec le bloque MySQLdump. Fin du script.")
-
+		print("Problème avec mysqldump lors du backup.")
+		exit(2)
 
 
 ### COPIE DES FICHIERS ###
@@ -87,11 +79,12 @@ def copy(files_in_site, temp_directory):
 		#	COPIE
 		# On fait le tour du tableau avec la boucle for.
 		print("")
-		print("Début de la copie des fichiers...")
+		print("Copie des fichiers...")
 		for file_or_dir in files_in_site:
-			# On contrôle si c'est un dossier ou un fichier. Un dossier = shutil.copytree(src, dst), un fichier = shutil.copyfile(src, dst).
-			# En dst, on indique le chemin du dossier de destination (logique) + le nom du dossier ou fichier à copier (logique mais moins évident). D'où l'utilisation d'os.path.basename !
-			# os.path.basename() nous renvoie le nom du dernier élément d'un chemin, et avec son extention. Donc ça nous donne 'wp-config.php' par exemple. Pratique !
+			# On contrôle si c'est un dossier ou un fichier.
+			# Un dossier = shutil.copytree(src, dst) ; un fichier = shutil.copyfile(src, dst).
+			# En dst, on indique le chemin du dossier de destination ET le nom du dossier ou fichier à copier. D'où l'utilisation d'os.path.basename.
+			# os.path.basename() nous renvoie le nom du dernier élément d'un chemin et avec son extention. Donc ça nous donne 'wp-config.php' par exemple.
 
 			# Si c'est un dossier et qu'il n'existe pas dans le dossier temporaire :
 			if os.path.isdir(file_or_dir) == True and os.path.exists(temp_directory + os.path.basename(file_or_dir)) == False:
@@ -108,12 +101,12 @@ def copy(files_in_site, temp_directory):
 			# Si anomalie :
 			else:
 				print("Il y a eu un problème avec" + file_or_dir)
-		print("Fin de la copie des fichiers.")
+		print("OK.")
 		print("")
 	except:
 		shutil.rmtree(temp_directory)
-		exit("Problème avec le bloque de copie.")
-
+		print("Problème de copie des fichiers lors du backup.")
+		exit(3)
 
 
 ### CRÉATION DU TAR.BZ2 ET NETTOYAGE DU DOSSIER TEMPORAIRE ###
@@ -121,28 +114,31 @@ def compress_clean(deb, temp_directory, name_of_backup):
 	try:
 		#	COMPRESSION
 		# Compression. Instruction pour le tar.bz2.
-		print("Début de la compression...")
+		print("Compression...")
 		with tarfile.open(deb + name_of_backup + '.tar.bz2', "w:bz2") as tar:
 			tar.add(temp_directory, os.path.basename(temp_directory))
-		print("Compression terminée.")
+		print("OK.")
 		print("")
 	except:
-		exit("Problème avec le bloque tarfile.")
+		print("Problème de compression lors du backup.")
+		exit(4)
 
 
 	try:
 		#	SUPPRESSION DOSSIER TEMPORAIRE
 		# Suppression du dossier temporaire.
 		shutil.rmtree(temp_directory)
-		print("Backup terminée avec succès !")
+		print("Backup OK !")
 	except:
-		exit("Problème avec le bloque de suppression du dossier temporaire.")
+		print("Problème de suppression du dossier temporaire lors du backup. Le fichier compressé est peut-être affecté.")
+		exit(5)
 
 
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### CODE DE LA PARTIE RESTAURATION ###
 
 
 
@@ -150,21 +146,20 @@ def compress_clean(deb, temp_directory, name_of_backup):
 def create_tmp_restore(deb, name_of_backup, temp_directory):
 
 	#	CONTRÔLE
-	# Y a-t-il déjà une archive du même nom ? Si on, exit. Sinon, c'est parfait, on continue.
+	# Y a-t-il déjà une archive du même nom ? Si non, exit. Si oui, on continue.
 	if not os.path.exists(deb + name_of_backup + '.tar.bz2'):
 		print("L'archive " + name_of_backup + ".tar.bz2 ne semble pas exister.")
-		exit(1)
+		exit(6)
 	# Au besoin, on créer le dossier temporaire, s'il est déjà là on ne fait rien.
 	if not os.path.exists(temp_directory):
 		os.mkdir(temp_directory)
-
 
 
 ### FONCTION POUR SIMULER LE CRASH ###
 def crash(files_in_site):
 	try:
 		# On fait le tour du tableau avec la boucle for.
-		print("Début de la suppression des fichiers existants...")
+		print("Remise à zéro...")
 		for file_or_dir in files_in_site:
 			# On contrôle si c'est un dossier ou un fichier. Un dossier = shutil.rmtree(), un fichier = os.remove().
 			# os.path.basename() nous renvoie le nom du dernier élément d'un chemin, et avec son extention. Donc ça nous donne 'wp-config.php' par exemple.
@@ -178,16 +173,15 @@ def crash(files_in_site):
 				print("Fichier supprimé : " + os.path.basename(file_or_dir))
 			# Si c'est un dossier ou un fichier mais qu'il n'existe pas dans /var, on fait un message :
 			elif os.path.exists(file_or_dir) == False:
-				print("Ce fichier ou dossier semble avoir déjà été supprimé : " + os.path.basename(file_or_dir))
+				print("Fichier ou dossier déjà supprimé : " + os.path.basename(file_or_dir))
 			# S'il y a anomalie :
 			else:
 				print("Il y a eu un problème avec " + file_or_dir)
-		print("Fin de la suppression.")
+		print("OK.")
 		print("")
 	except:
 		shutil.rmtree(temp_directory)
-		exit("Problème avec le bloque de suppression.")
-
+		exit(7)
 
 
 ### EXTRATION ###
@@ -195,22 +189,21 @@ def extract(deb, name_of_backup, temp_directory):
 	try:
 		#	DÉCOMPRESSION
 		# Décompression. Instruction pour le tar.bz2.
-		print("Début de la décompression...")
+		print("Décompression...")
 		with tarfile.open(deb + name_of_backup + '.tar.bz2') as tar:
 			tar.extractall(temp_directory)
-		print("Décompression terminée.")
+		print("OK.")
 		print("")
 	except:
 		shutil.rmtree(temp_directory)
-		exit("Problème lors de la décompression. Fin du script.")
-
+		exit(8)
 
 
 ### RESTAURATION ###
 def restauration(files_to_restore, site_directory, temp_directory):
 	try:
 		#	RESTAURATION
-		print("Début de la restauration des fichiers...")
+		print("Restauration...")
 		for file_or_dir in files_to_restore:
 			# On contrôle si c'est un dossier ou un fichier. Un dossier = shutil.rmtree(), un fichier = os.remove().
 			# Si c'est un dossier et qu'il n'existe pas dans /var, on le met dans /var :
@@ -227,30 +220,29 @@ def restauration(files_to_restore, site_directory, temp_directory):
 			# S'il y a anomalie :
 			else:
 				print("Il y a eu un problème avec " + os.path.basename(file_or_dir))
-		print("Fin de la restauration des fichiers.")
+		print("OK.")
 		print("")
 	except:
-		exit("Problème avec le bloque de restauration.")
-
+		exit(9)
 
 
 ### SQL_DUMP ###
 def sql_restore(DB_HOST, DB_USER, DB_USER_PASSWORD, DB_NAME, BACKUP_PATH):
 	try:
 		#	MYSQLDUMP
-		print("Début de la restauration MySQL...")
+		print("Restauration de la base MySQL...")
 		# La ligne de code qui sera exécutée par subprocess.
 		dumpcmd = "mysql -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + DB_NAME + " < " + BACKUP_PATH + DB_NAME + ".sql"
 		subprocess.run(dumpcmd, shell=True)
 		# Un print pour voir ce qui est clairement saisie.
-		print("Fin de la restauration MySQL.")
+		print("OK.")
 	except:
 		# MySQLdump peut générer des erreurs mais poursuivre correctement malgré tout...
-		exit("Problème avec le bloque MySQLdump. Le script continue quand même.")
+		exit(10)
 
 	shutil.rmtree(temp_directory)
 	print("")
-	print("Fin de la restauration.")
+	print("Restauration OK.")
 
 
 
@@ -267,6 +259,7 @@ def backup():
 	copy(files_in_site, temp_directory)
 	compress_clean(deb, temp_directory, name_of_backup)
 
+
 # Appels des fonctions pour restaurer.
 def restore():
 	create_tmp_restore(deb, name_of_backup, temp_directory)
@@ -275,51 +268,45 @@ def restore():
 	restauration(files_to_restore, site_directory, temp_directory)
 	sql_restore(DB_HOST, DB_USER, DB_USER_PASSWORD, DB_NAME, BACKUP_PATH)
 
-vars = readConf(sys.argv[1])
 
-# Les constantes.
-# Constante du chemin de home.
-deb = vars['constantes']['deb']
-# Nom de la backup.
-name_of_backup = vars['constantes']['name_of_backup']
-# Constante du chemin du dossier temporaire, on ne peut pas faire d'assemblage de variable dans un .yaml.
-temp_directory = deb + name_of_backup + '/'
-# Tableau qui contient les fichiers à sauvegarder ou supprimer selon la situation.
-files_in_site = vars['constantes']['files_in_site']
-# Tableau qui contient les fichiers à remettre en place.
-files_to_restore = [temp_directory + 'wp-config.php',temp_directory + 'wp-content',temp_directory + '.htaccess']
+try:
+	vars = readConf(sys.argv[1])
+except:
+	print("La fonction readConf ne retourne pas les données, problème avec le fichier yaml.")
+	exit(11)
 
-# Les constantes MySQL.
-DB_HOST = vars['constantes']['DB_HOST']
-DB_USER = vars['constantes']['DB_USER']
-DB_USER_PASSWORD = vars['constantes']['DB_USER_PASSWORD']
-DB_NAME = vars['constantes']['DB_NAME']
-BACKUP_PATH = deb + name_of_backup + '/'
 
-# Constante du chemin du dossier du site.
-site_directory = vars['constantes']['site_directory']
-
+try:
+	# Les constantes.
+	# Chemin de home.
+	deb = vars['constantes']['deb']
+	# Nom de la backup.
+	name_of_backup = vars['constantes']['name_of_backup']
+	# Chemin du dossier temporaire, on ne peut pas faire d'assemblage de variable dans un .yaml.
+	temp_directory = deb + name_of_backup + '/'
+	# Chemin du dossier du site.
+	site_directory = vars['constantes']['site_directory']
+	# Tableau qui contient les fichiers à sauvegarder ou supprimer selon la situation.
+	files_in_site = vars['constantes']['files_in_site']
+	# Tableau qui contient les fichiers à remettre en place.
+	files_to_restore = [temp_directory + 'wp-config.php',temp_directory + 'wp-content',temp_directory + '.htaccess']
+	# Les constantes MySQL.
+	DB_HOST = vars['constantes']['DB_HOST']
+	DB_USER = vars['constantes']['DB_USER']
+	DB_USER_PASSWORD = vars['constantes']['DB_USER_PASSWORD']
+	DB_NAME = vars['constantes']['DB_NAME']
+	BACKUP_PATH = temp_directory
+	# BACKUP_PATH original :
+	# BACKUP_PATH = deb + name_of_backup + '/'
+except:
+	print("Problème dans les constantes, fichier yaml à vérifier.")
+	exit(12)
 
 
 if (vars['constantes']['status'] == 'restore'):
 	restore()
 else:
 	backup()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
