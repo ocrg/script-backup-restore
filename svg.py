a27ef8ba9b850faa6814e25614909fac4b557ad1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-08 18:04:35 +0000

    Adding tarfile member sanitization to extractall()

--- svg.py
+++ svg.py
@@ -202,7 +202,26 @@ def extract(deb, name_of_backup, temp_directory):
 		print("Décompression...")
 		with tarfile.open(deb + name_of_backup + '.tar.bz2') as tar:
 			# .extractall pour décompresser.
-			tar.extractall(temp_directory)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, temp_directory)
 		print("OK.")
 		print("")
 	except:
